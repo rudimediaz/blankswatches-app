5787350f1c4538e968224ae3782be327b308a4c8
Rudhi Mediastara <42732580+rudimediaz@users.noreply.github.com>
2022-07-01 15:50:38 +0000

    sty;e: move from goober to css modules

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,33 +1,12 @@
-import { css } from 'goober';
 import type { Component } from 'solid-js';
 import IntegratedSwatches from '../components/IntegratedSwatches';
-
-const ctn = css({
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3,1fr)',
-  gridTemplateRows: 'minmax(100vh,1fr)',
-});
-
-const swatches = css({
-  gridArea: '1/1/2/4',
-  '@media (min-width : 1024px)': {
-    gridArea: '1/1/2/3',
-  },
-});
-
-const sidebar = css({
-  display: 'none',
-  '@media (min-width : 1024px)': {
-    gridArea: '1/3/2/4',
-    display: 'grid',
-  },
-});
+import c from './MainView.module.css';
 
 const MainView: Component = () => {
   return (
-    <div class={ctn} data-testid="main-view">
-      <IntegratedSwatches class={swatches} />
-      <div class={sidebar}>sidebar</div>
+    <div class={c.ctn} data-testid="main-view">
+      <IntegratedSwatches class={c.swatches} />
+      <div class={c.sidebar}>{''}</div>
     </div>
   );
 };
