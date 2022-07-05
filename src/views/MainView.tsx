da40ee168d4a5ecb67fe06282b96a3febd8848ef
Rudhi Mediastara <42732580+rudimediaz@users.noreply.github.com>
2022-07-05 06:04:20 +0000

    feat: hsl wrapper

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,16 +1,19 @@
 import type { Component } from 'solid-js';
+import CurrentHSL from '../components/CurrentHSL';
 import IntegratedSwatches from '../components/IntegratedSwatches';
 import SidePane from '../components/SidePane';
 import c from './MainView.module.css';
 
 const MainView: Component = () => {
   return (
-    <div class={c.ctn} data-testid="main-view">
-      <IntegratedSwatches class={c.swatches} />
-      <div class={c.sidebar}>
-        <SidePane />
+    <CurrentHSL>
+      <div class={c.ctn} data-testid="main-view">
+        <IntegratedSwatches class={c.swatches} />
+        <div class={c.sidebar}>
+          <SidePane />
+        </div>
       </div>
-    </div>
+    </CurrentHSL>
   );
 };
 
